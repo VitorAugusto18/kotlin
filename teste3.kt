f33aefd462bc7d59f3219b114039d06846b06339
Vitor Augusto dos Santos <vitoraugusto18@hotmail.com>
2022-10-19 23:22:02 -0300

    Update teste3.kt
    Testando uso do Calendar e mudança do uso do when para if

--- teste3.kt
+++ teste3.kt
@@ -1,7 +1,11 @@
+import java.util.Calendar
 import javax.swing.JOptionPane
 //Teste com uso do condicional when
 fun main(Args: Array<String>){
-    val ger1 = "Baby Boomers"
+    var date = Calendar.getInstance()
+    var anoAtual = date.get(Calendar.YEAR)
+    anoAtual.toInt()
+    val ger1 = "Baby Boomer"
     val ger2 = "X"
     val ger3 = "Y"
     val ger4 = "Z"
@@ -15,12 +19,22 @@ fun main(Args: Array<String>){
     if(opcaoBot == JOptionPane.YES_OPTION){
         var born = JOptionPane.showInputDialog(null,"Qual o ano do seu nascimento?").toInt()
 
-        when(born){
+        if(born >=1945 && born <= 1964){
+            JOptionPane.showMessageDialog(null, "Você pertence a geração " +ger1)
+        }else if(born > 1964 && born <= 1984){
+            JOptionPane.showMessageDialog(null, "Você pertence a geração " + ger2)
+        }else if(born > 1984 && born <= 1999){
+            JOptionPane.showMessageDialog(null,"Você pertence a geração " + ger3)
+        }else if(born > 2000 && born <= anoAtual){
+            JOptionPane.showMessageDialog(null,"Você pertence a geração "+ ger4)
+        }else (JOptionPane.showMessageDialog(null, "Sem informação"))
+        /*when(born){
             in 1945..1964 -> JOptionPane.showMessageDialog(null, "Você pertence a geração " +ger1)
             in 1965..1984 -> JOptionPane.showMessageDialog(null, "Você pertence a geração " + ger2)
-            in 1985..1999  -> JOptionPane.showMessageDialog(null,"Você pertence a geração " + ger3)
-            else -> JOptionPane.showMessageDialog(null, ger4)
-        }
+            in 1985..1999 -> JOptionPane.showMessageDialog(null,"Você pertence a geração " + ger3)
+            in 2000 < .. <2022 -> JOptionPane.showMessageDialog(null,"Você pertence a geração "+ ger4)
+            else -> JOptionPane.showMessageDialog(null, "Sem informação")
+        }*/
     }else if(opcaoBot == JOptionPane.NO_OPTION){
         JOptionPane.showMessageDialog(null,"Execução encerrada")
         System.exit(0)
