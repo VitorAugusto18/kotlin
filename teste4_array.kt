22eb971d84ece554a1ac19d19b54b2e652ccac26
Vitor Augusto dos Santos <vitoraugusto18@hotmail.com>
2022-10-23 21:20:16 -0300

    Update teste4_array.kt
    comentando linhas

--- teste4_array.kt
+++ teste4_array.kt
@@ -3,16 +3,15 @@ import java.util.*
 fun main (args: Array<String>){
 
     val paises = arrayOf("Brasil", "Chile", "Angola", "Canada","EUA")
-    paises.sort()
+    paises.sort() //ordenação alfabética da lista de países
     println("Qual país você quer saber a posição na lista?")
     var posPais = readLine().toString()
     val posicao = Arrays.binarySearch(paises, posPais)
     val tamanho = paises.count()
-    //paises.reverse()
-    //paises.sort()
+    //paises.reverse() -> invertendo a ordem da lista
     //paises.sortBy{it.length >6 }
     /*paises.forEach{
         println("$it")
-    }*/
+    } usando o laço for each para trazer ordenação da lista de acordado com o solicitado no sortBy*/
     println("O país "+posPais+" está na posição "+posicao+ " de uma lista de tamanho "+tamanho)
 }
